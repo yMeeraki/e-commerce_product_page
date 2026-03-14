76e5010f8075af44b9369780ccc9e896178c917e
Yukti Gupta <yuktigupta95@gmail.com>
2026-03-14 13:26:57 +0530

    Responsive Slideshow Gallery using jQuery

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-$(document).ready
+$(document).ready(function () {
+  // Responsive Slideshow Gallery using jQuery
+  let current = 0;
+  let productSlides = $(".product_gallery-slide");
+  console.log(productSlides);
+
+  function showProductImage(index) {
+    productSlides.removeClass("active");
+    productSlides.eq(index).addClass("active");
+  }
+
+  $(".next").click(function () {
+    current++;
+
+    if (current >= productSlides.length) {
+      current = 0;
+    }
+    showProductImage(current);
+  });
+
+  $(".prev").click(function () {
+    current--;
+
+    if (current < 0) {
+      current = productSlides.length - 1;
+    }
+    showProductImage(current);
+  });
+});
